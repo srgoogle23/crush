fd57a834d825a31803908c091ae9db3d7ffd2322
Leonardo Oliveira <leonardoliveira@ufmg.br>
2022-11-07 23:22:37 -0300

    feat: add files

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Coisas de amor",
+    introTitle: 'Carolina Assis',
+    introDesc: `Introdução`,
+    btnIntro: 'Oi amor!',
+    title: 'Você se apaixonou à primeira vista 🥰',
+    desc: 'Você ficou doida desde o momento em que viu aquele sorriso ',
+    btnYes: 'Verdadeiro',
+    btnNo: 'Falso',
+    question: 'question',
+    btnReply: 'replybtn',
+    reply: 'reply',
+    mess: 'Eu sei disso 🥰. Te amo muito 😘😘',
+    messDesc: 'Vou buscá-lo hoje às 19h',
+    btnAccept: 'ok',
+    messLink: 'https://wa.me/5531999079939'
+}
